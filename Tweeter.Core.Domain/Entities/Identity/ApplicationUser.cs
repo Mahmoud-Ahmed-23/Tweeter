bf7a1029d3ad981ecc06cafc84e2e6829bc311ca
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2025-04-28 09:23:56 +0300

    Add project files.

---
+++ Tweeter.Core.Domain/Entities/Identity/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tweeter.Core.Domain.Entities.Identity
+{
+	public class ApplicationUser : IdentityUser
+	{
+		public required string FullName { get; set; }
+
+	}
+}
