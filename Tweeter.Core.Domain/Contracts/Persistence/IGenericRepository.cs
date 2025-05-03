b925573636523715adff435691e706545f640d60
mohamed_hamdi_mahmoud <mohammedhamdi726@gmail.com>
2025-05-03 05:52:33 +0300

    Implement GenericRepository

---
+++ Tweeter.Core.Domain/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using Tweeter.Core.Domain.Common;
+
+namespace Tweeter.Core.Domain.Contracts.Persistence
+{
+    public interface IGenericRepository<TEntity, TKey>
+       where TEntity : BaseEntity<TKey> where TKey : IEquatable<TKey>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(bool WithTraching = false);
+
+        Task<TEntity?> GetAsync(TKey id);
+
+
+        IQueryable<TEntity> GetQueryable();
+
+
+
+        Task AddAsync(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+
+    }
+}
