a1363bfc47a49d0c0b8150a91c9790766c10e953
mohamed_hamdi_mahmoud <mohammedhamdi726@gmail.com>
2025-06-28 06:57:49 +0300

    Implement Get All Hashtages Sevice with Paginated ,Ordring Based Count Of Tweets Count And Implement Serch For Specific Hashtag And  Sovle Confilite With GitHub

---
+++ Tweeter.Core.Application/Features/Hashtages/Queries/Models/GetAllHashtagesQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Tweeter.Core.Application.Abstraction.Common;
+using Tweeter.Core.Application.Abstraction.Dtos.Hashtags;
+using Tweeter.Core.Application.Bases;
+
+namespace Tweeter.Core.Application.Features.Hashtages.Queries.Models
+{
+    public class GetAllHashtagesQuery : IRequest<Response<Pagination<HashtagToReturn>>>
+    {
+        public SpecParams SpecParams { get; set; }
+
+        public GetAllHashtagesQuery(SpecParams specParams)
+        {
+            SpecParams = specParams;
+        }
+
+    }
+}
